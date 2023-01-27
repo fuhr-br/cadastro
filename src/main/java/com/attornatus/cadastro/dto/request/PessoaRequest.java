b6733188371d6815919deb2f4c9b5dc3ce22f277
anderson fuhr souza <andersonfuhr@yahoo.com.br>
2023-01-27 19:12:17 -0300

    remoção imports não utilizados

--- src/main/java/com/attornatus/cadastro/dto/request/PessoaRequest.java
+++ src/main/java/com/attornatus/cadastro/dto/request/PessoaRequest.java
@@ -1,9 +1,7 @@
 package com.attornatus.cadastro.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
--- src/main/java/com/attornatus/cadastro/dto/response/PessoaResponse.java
+++ src/main/java/com/attornatus/cadastro/dto/response/PessoaResponse.java
@@ -1,9 +1,7 @@
 package com.attornatus.cadastro.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
